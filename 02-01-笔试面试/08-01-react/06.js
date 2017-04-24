da35ed620b085e8729805a6a68d5d29302d8805b
zhanglongde <1039326495@qq.com>
2017-04-25 00:17:25 +0800

    1)前端面试

--- 02-01-笔试面试/08-01-react/06.js
+++ 02-01-笔试面试/08-01-react/06.js
@@ -4,3 +4,19 @@
 
 import React, {Component,PropTypes} from 'react'
 import fetchUser from 'twitter'
+
+class Twitter extends Component {
+  state = {
+    user:null,
+  }
+  static propTypes = {
+    userName:PropTypes.string.isRequired,
+  }
+  componentDidMount () {
+    fetchUser(this.props.username)
+      .then((user) => this.setState({user}))
+  }
+  render () {
+    return this.props.children(this.state.user)
+  }
+}
