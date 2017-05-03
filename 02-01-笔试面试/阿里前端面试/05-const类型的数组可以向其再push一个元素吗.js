21d01d3450ae46511345fde3ee82f5120655e629
zhanglongde <1039326495@qq.com>
2017-05-03 14:25:27 +0800

    1)深度补充

---
+++ 02-01-笔试面试/阿里前端面试/05-const类型的数组可以向其再push一个元素吗.js
@@ -0,0 +1,17 @@
+/**
+ * Created by JUEXINPC-008 on 2017/5/3.
+ */
+
+const arr = [1,2,3]
+arr.push(5)
+console.log(arr)
+
+arr[1] = 11234
+console.log(arr)
+
+const obj = {
+    a:12
+}
+console.log(obj)
+obj.a = 1234
+console.log(obj)
