389443193bcac509d335363d3bf50168db3cbf99
zhanglongde <1039326495@qq.com>
2017-08-04 18:35:30 +0800

    1）算法与数据结构 2）邮件折叠效果

---
+++ 00-01-随意练习/signature.js
@@ -0,0 +1,5 @@
+/**
+ * Created by JUEXINPC-008 on 2017/8/4.
+ */
+
+var signatureHtml = '\u003cdiv dir=\"ltr\"\u003e\u003cdiv dir=\"ltr\"\u003e\u003cdiv dir=\"ltr\"\u003e\u003cdiv dir=\"ltr\"\u003e\u003cdiv\u003e\u003ca href=\"mailto:longde.zhang@nextcont.com\" style=\"font-family:verdana;font-size:13px\" target=\"_blank\"\u003elongde.zhang@nextcont.com\u003c/a\u003e 觉新 \u003c/div\u003e\u003c/div\u003e\u003c/div\u003e\u003c/div\u003e\u003c/div\u003e'
