aaa0ee7316f73941862b39d26acc0e52e2dfe994
zhanglongde <1039326495@qq.com>
2017-11-10 16:54:25 +0800

    test scroll and sticky footer

--- 01-01-前端基本功/6-用户体验/05-滚动条/滚动条与sticky/mailData.js
+++ 01-01-前端基本功/6-用户体验/05-滚动条/滚动条与sticky/mailData.js
@@ -2,6 +2,98 @@
  * Created by JUEXINPC-008 on 2017/11/10.
  */
 
-(function () {
+var thread = {
+    messages:[
+        {
+            id:1,
+            from:['zld'],
+            draft: {},
+            isOpen: false,
+            html:`                Min-height
 
-}());
+                The #container element of this page has a min-height of 100%. That way, if the content requires more height than the viewport provides, the height of #content forces #container to become longer as well. Possible columns in #content can then be visualised with a background image on #container; divs are not table cells, and you don't need (or want) the fysical elements to create such a visual effect. If you're not yet convinced; think wobbly lines and gradients instead of straight lines and simple color schemes.
+
+                Relative positioning
+
+                Because #container has a relative position, #footer will always remain at its bottom; since the min-height mentioned above does not prevent #container from scaling, this will work even if (or rather especially when) #content forces #container to become longer.
+
+                Padding-bottom
+
+                Since it is no longer in the normal flow, padding-bottom of #content now provides the space for the absolute #footer. This padding is included in the scrolled height by default, so that the footer will never overlap the above content.
+
+                Scale the text size a bit or resize your browser window to test this layout. The CSS file is over here.
+
+                Back to CSS Examples
+                er: My late wife worked in loss prevention for Walmart for about seven yea.​.​.​
+                Quora
+                Your Reading Digest
+                TOP STORIES FOR YOU
+                How often does theft occur at Walmart?
+                Tim Dees
+                Tim Dees, criminal justice technology writer and consultant
+                Written Jun 18, 2015 · Featured on Business Insider
+                My late wife worked in loss prevention for Walmart for about seven years. She was what Walmart calls an "in-store" for three years, patrolling the store in street clothes a... Read More »
+                What would Einstein think of programming?
+                Al Klein
+                Al Klein, 45 years of earning a living developing systems.
+                Written Jun 26
+                I spoke to Einstein (yes, I’m that old), but didn’t know about computers, so I never asked him for his opinion of them. But I’d hazard a guess that he’d find the concept in... Read More »
+                Is Facebook react JS just a hype or do they really have something there?
+                Ken Mazaika
+                Ken Mazaika, CTO, Co-founder & Code Mentor @ The Firehose Project
+                Written 11h ago
+
+                ReactJS has a ton of hype. Like Malcolm Gladwell’s Tipping Point, where he analyzed the then-terminally-uncool Hush Puppies shoes into a popular brand hype played a role. Bu... Read More »
+                Can an all you can eat buffet restaurant tell me to leave if I ate there the whole day?
+                Martin Bayer
+                Martin Bayer, I have over 20 years of kitchen experience.
+                Written Feb 6
+                I attempted this on one occasion when I was a kid. Some friends and I decided to go to Old Country Buffet during lunch time and try to stick around for dinner, and we weren... Read More »
+                Should I change my website from HTTP to HTTPS ?
+                David Bernier
+                David Bernier, Founder at David Bernier Media
+                Written Mon
+                A month ago my answer would have been “No” but since Google’s latest update, on November 1st 2017, now its a definitive “YES”. Before the update, keeping your site on HTTP h... Read More »
+                I know HTML, CSS, and a bit of JavaScript. What should I learn next?
+                Thanh Phong
+                Thanh Phong
+                Written Aug 12, 2015
+                Get a job. Seriously, the only reason you think you finished learning those technologies is because you are not working as a web developer. ... Read More »
+                Does Microsoft know that no one wants Windows 10 S?
+                Mark Phaedrus
+                Mark Phaedrus, works at Microsoft
+                Written Jun 15
+                Come over here. I’m gonna level with you about a few things. Come on. Closer. Just between us. You wanna know what Microsoft likes? An awful lot? Money.... Read More »
+                Where is the safest place in the U.S? What is the safest place in the world?
+                Andrew S.E. Erickson
+                Andrew S.E. Erickson, Former U.S. Diplomat
+                Updated Jul 5
+                You’re probably aware of the tale of the British family that wanted to move to the safest place in the world, a place free of all military threats. They did extensive resea... Read More »
+                What are the best laptops for developers?
+                Titus Iudean
+                Titus Iudean, Bashed code on Macs and ThinkPads
+                Written Oct 30
+                I have answered this before and I very much still abide by what I said then. To save you clicking a link here is my advice again: Nowadays most laptops are good for most pro... Read More »
+                Why should I write JavaScript on the server side?
+                Ilya Goncharov
+                Ilya Goncharov, JavaScript, HTML, CSS. Here's my account on CodeWars: bit-ly/23Z873l
+                Written Wed
+                As someone working for a software outsourcing company, having one language to power both the client side and the backend is more productive. I’ve listed some of the benefit... Read More »
+                Read More in Your Feed
+                Follow Quora on:
+                FacebookTwitter
+                Get the App for iOS and Android
+                Quora
+                This email was sent by Quora (650 Castro Street #450, Mountain View, CA 94041).
+                If you don't want to receive these emails in the future, please unsubscribe.`},
+        {
+            id:2,
+            from:['zlb'],
+            draft:{},
+            isOpen: false,
+            html:`                Min-height
+
+                The #container element of this page has a min-height of 100%. That way, if the content requires more height than the viewport provides, the height of #content forces #container to become longer as well. Possible columns in #content can then be visualised with a background image on #container; divs are not table cells, and you don't need (or want) the fysical elements to create such a visual effect. If you're not yet convinced; think wobbly lines and gradients instead of straight lines and simple color schemes.
+`}
+    ]
+}
