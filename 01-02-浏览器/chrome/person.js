8407e8662203b5d589d2cd9c49a26851dd36c0ca
zhanglongde <1039326495@qq.com>
2017-12-28 00:41:04 +0800

    1.模板编译 2.async递归与Promise.all 3.with应用 4.import动态导入

---
+++ 01-02-浏览器/chrome/person.js
@@ -0,0 +1,6 @@
+
+function Person(name) {
+  this.name = name
+}
+
+export default Person;
--- 01-02-浏览器/chrome/新特性/dialogBox.js
+++ 01-02-浏览器/chrome/新特性/dialogBox.js
@@ -2,8 +2,13 @@
  * Created by Administrator on 2017/11/17.
  */
 
+import Person from'../person'
+
 export function dialogBox() {
   this.open = function () {
     console.log('new box...')
+
+    let p = new Person('xxx')
+    console.log(p.name)
   }
 }
