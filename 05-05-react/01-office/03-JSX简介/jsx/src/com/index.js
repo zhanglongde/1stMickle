d784925e88e5f4c16a760cf18b7156f85854107b
zhanglongde <1039326495@qq.com>
2018-11-19 00:13:01 +0800

    模块化进阶

--- 05-05-react/01-office/03-JSX简介/jsx/src/com/index.js
+++ 05-05-react/01-office/03-JSX简介/jsx/src/com/index.js
@@ -1,5 +1,5 @@
 import Button from './Button'
 import Tab from './Tab'
 
-
 export {Button, Tab}
+export default {Button, Tab} // 指定默认输出，引用的时候不需要知道具体名称
--- 05-05-react/01-office/03-JSX简介/jsx/src/stringToComponent.jsx
+++ 05-05-react/01-office/03-JSX简介/jsx/src/stringToComponent.jsx
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 
 // import Button from './com/Button';
 // const {Tab} = require('./com')
-const _antd = require("./com");
+// const _antd = require("./com");
+import * as _antd from './com';
+// import _antd from './com';
+// import Button, Tab as _antd from './com';
+// import {Button, Tab} as _antd from './com';
 
 const _interopRequireDefault = obj => {
+  console.log(obj)
   return obj && obj.__esModule ? obj : { default: obj };
 };
 
@@ -15,6 +20,7 @@ const arr = ['Button', 'Tab'];
 export default class MyComp extends Component {
   renderComp = arr => {
     return arr.map(v => {
+      console.log(_antd2)
       return React.createElement(_antd2[v], null, 123);
       //<Input /> 设置children为字符串会报 xss错误 所以改成两个<Button />了
     })
