f51ef4d7e2fe804ea592b5d83536ec20cc1fde48
zhanglongde <1039326495@qq.com>
2019-04-20 23:57:37 +0800

    record

---
+++ 13-前端测试/puppeteer/puppeteer-recorder/record.js
@@ -0,0 +1,25 @@
+const puppeteer = require('puppeteer');
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false
+  })
+  const page = await browser.newPage()
+
+  const navigationPromise = page.waitForNavigation()
+
+  await page.goto('https://www.jianshu.com/p/56babda610f9')
+
+  await page.setViewport({ width: 1920, height: 937 })
+
+  await page.waitForSelector('.post > .article > .show-content > .show-content-free > p:nth-child(1)')
+  await page.click('.post > .article > .show-content > .show-content-free > p:nth-child(1)')
+
+  await page.waitForSelector('.article > .author > .info > .name > a')
+  await page.click('.article > .author > .info > .name > a')
+
+  await navigationPromise
+  
+
+//   await browser.close()
+
+})()
