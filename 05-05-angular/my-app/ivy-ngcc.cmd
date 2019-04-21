371839e9f0aab703c742e769bbaac0716596ef8c
zhanglongde <1039326495@qq.com>
2019-04-21 22:49:12 +0800

    del

--- 05-05-angular/my-app/ivy-ngcc.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\node_modules\@angular\compiler-cli\src\ngcc\main-ngcc.js" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\node_modules\@angular\compiler-cli\src\ngcc\main-ngcc.js" %*
-)
