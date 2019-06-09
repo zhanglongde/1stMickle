2b4ae37313538e5d4732202de5c2fcb9daf845da
zhanglongde <1039326495@qq.com>
2019-06-09 22:29:30 +0800

    大屏解决方案 数据产品 reacthooks

---
+++ 00-02-前端专业方向/00-04-性能调优/WebWorker/worker.js
@@ -0,0 +1,5 @@
+self.onmessage = function () {
+//   const start = performance.now()
+//   while (performance.now() - start < 1000) {}
+  postMessage('done!')
+}
--- 00-03-前端深度问题/04-04-react/React Hooks/ReactHooks.md
+++ 00-03-前端深度问题/04-04-react/React Hooks/ReactHooks.md
@@ -1,6 +1,7 @@
 应用场景
 1.渲染、状态分离
+  纯函数组件
 2.utils库
-React Hooks
+React Hooks升级recompose
 3.状态管理
 先定义 model，再以 Hooks 的方式消费
